16aee3b01ce798fd14cbd28a6e06b59373d350b2
Mark <mphines@gmail.com>
2014-02-27 19:37:41 -0600

    first ruby app commit

---
+++ award-show-app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwardShowApp::Application.config.secret_key_base = '6abe0b2945752f1b716aab70f02672aecf15c1a3282bc7c0760ed67d4bf13af88e84f7daa5e8a9a1984196a18030cce2033ee8d1066a11bb0917b8d2e11a4e42'
